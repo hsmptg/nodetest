743d50c7a3a34120edeb6fe4aca2dcca73efbecf
Macbook <hsmptg@gmail.com>
2016-04-15 21:49:43 +0100

    Add RPi hardware

--- app.js
+++ app.js
@@ -2,10 +2,38 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var os = require('os');
+
+if (os.cpus()[0].model.indexOf('ARM') > -1) {
+    var Gpio = require('pigpio').Gpio,
+        button = new Gpio(23, {
+            mode: Gpio.INPUT,
+            pullUpDown: Gpio.PUD_UP,
+            edge: Gpio.EITHER_EDGE
+        }),
+        led = new Gpio(16, {mode: Gpio.OUTPUT});
+
+    button.on('interrupt', function (level) {
+        console.log('but= ' + level);
+        io.emit('but', { state: level });
+    });
+}
+else {
+    var state = true;
+    function tick() {
+        console.log('but= ' + state);
+        io.emit('but', { state: state });
+        state = !state;
+    }
+    setInterval(tick, 3000);
+}
 
 io.on('connection', function(socket) {
     socket.on('led', function(data) {
         console.log(data);
+        console.log(Gpio);
+        if (Gpio)
+            led.digitalWrite(level);
     });
 });
 
@@ -17,14 +45,6 @@ app.set('views', __dirname + '/views');
 app.get('/', function(req, res) {
     res.render('index');
 });
-
-var state = true;
-function tick() {
-    console.log('but= ' + state);
-    io.emit('but', { state: state });
-    state = !state;
-}
-setInterval(tick, 3000);
 
 server.listen(3000, function () {
     console.log('Example app listening on port 3000!');
